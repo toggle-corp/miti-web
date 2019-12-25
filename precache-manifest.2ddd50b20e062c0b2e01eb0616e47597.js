3d3550d86d81f791799e4fac1ed5d2b8d14a8f02
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-25 14:52:10 +0000

    Deploy toggle-corp/miti-web to github.com/toggle-corp/miti-web.git:gh-pages

---
+++ precache-manifest.2ddd50b20e062c0b2e01eb0616e47597.js
@@ -0,0 +1,41 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027b1028e149b0b3815b40e273102ac2",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "b5995ed8ceff6a7b6d94",
+    "url": "/css/1.css"
+  },
+  {
+    "revision": "32e0469e823f2764f9dc",
+    "url": "/css/3.css"
+  },
+  {
+    "revision": "9f4f21aa50fb64eb0738",
+    "url": "/css/4.css"
+  },
+  {
+    "revision": "319d1784a66fbffcb2b0",
+    "url": "/css/5.css"
+  },
+  {
+    "url": "/js/3.32e0469e823f2764f9dc.js"
+  },
+  {
+    "url": "/js/4.9f4f21aa50fb64eb0738.js"
+  },
+  {
+    "url": "/js/5.319d1784a66fbffcb2b0.js"
+  },
+  {
+    "url": "/js/main.b5995ed8ceff6a7b6d94.js"
+  },
+  {
+    "revision": "ffd76faf0fe2aefe7a2e",
+    "url": "/js/runtime.ab3388ec8866eede859b.js"
+  },
+  {
+    "url": "/js/vendors.e5ff7888fa6c10fd047d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.2ddd50b20e062c0b2e01eb0616e47597.js"
+);
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.precaching.cleanupOutdatedCaches();
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/assets/,/^\/admin/,/^\/api/],
+});
+
+workbox.routing.registerRoute(/assets/, new workbox.strategies.CacheFirst(), 'GET');
